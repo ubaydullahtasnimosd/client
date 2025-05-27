7fd018c0269a6b1a46dbdb8ef276d92982dd170f
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-05-28 05:15:53 +0600

    feat(vercel.json): added

--- pages/Miscellaneous.jsx
+++ pages/Miscellaneous.jsx
@@ -41,7 +41,7 @@ export const Miscellaneous = () => {
           বিবিধ ভিডিও
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {data?.map((item) => (
             <div
               key={item.id}
