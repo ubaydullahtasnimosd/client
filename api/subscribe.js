7a61743275f1586062fb58daac18aec6db4c75b3
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-07-18 14:55:51 +0600

    feat(subscribe.js) : API_BASE_URL live link added

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://server-iota-ebon-83.vercel.app/api/v1/subscribe";
+const API_BASE_URL = "https://ubaydullah-tasnim-backend.vercel.app/api/v1/subscribe";
 
 export const subscribeEmail = async (formData) => {
   const response = await axios.post(`${API_BASE_URL}/subscribe/`, formData);
